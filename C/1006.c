3b0b79aaf9b6e1d69f94b2f95423485120a4ef88
Mateus Henrique <51799470+mateus-maximo@users.noreply.github.com>
2021-06-02 00:58:16 -0300

    add

---
+++ C/1006.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    double A, B, C;
+
+    scanf("%lf\n%lf\n%lf", &A, &B, &C);
+    
+    double media = ((A*2) + (B*3) + (C*5)) / 10;
+
+    printf("MEDIA = %.1lf\n", media);
+
+    return 0;
+}
