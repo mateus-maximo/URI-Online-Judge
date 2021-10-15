450b49f28e50b1b20425ed7392d859826023cf1b
Mateus <mateushen19@outlook.com>
2021-10-15 19:07:33 -0300

    Adicionando exercícios

---
+++ JavaScript/1006.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+var A = parseFloat(lines.shift())
+var B = parseFloat(lines.shift())
+var C = parseFloat(lines.shift())
+
+var media = ((A * 2) + (B * 3) + (C * 5))/10
+
+console.log(`MEDIA = ${media.toFixed(1)}`)
